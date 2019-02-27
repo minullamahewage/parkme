272a69308b4cfc6b2adef7105843b8a699a51354
hiranlowe <w.n.r.lowe@gmail.com>
2019-02-27 11:28:10 +0530

    b

--- maps.js
+++ maps.js
@@ -15,6 +15,7 @@ var platform = new H.service.Platform({
       zoom: 14,
       center: { lng: 79.861788, lat:6.913887  }
     });
+    //var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
     // Create a marker icon from an image URL:
     var icon = new H.map.Icon('assets/graphics/parksymbol1.png');
 
@@ -23,6 +24,19 @@ var platform = new H.service.Platform({
 
     // Add the marker to the map:
     map.addObject(marker);
+//get current location
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+function showPosition(position) {
+    this.lat = position.coords.latitude;
+    this.lng = position.coords.longitude;
+    //return lat+ "," + lng;
+  }
 //reading xml file
 function getDistance(){
     var xhttp = new XMLHttpRequest();
@@ -31,7 +45,9 @@ function getDistance(){
         readDistance(this);
         }
     };
-    xhttp.open("GET", "https://route.api.here.com/routing/7.2/calculateroute.xml?app_id=EaMacGi1Wj3dRmQlGXCu&app_code=YXiH50dmZNPxmzS0ldy6Sw&waypoint0=geo!6.795043,79.900576&waypoint1=geo!6.911750,79.851406&mode=fastest;car;traffic:disabled", true);
+    var lat = showPosition().lat;
+    var lng = showPosition().lng;
+    xhttp.open("GET", "https://route.api.here.com/routing/7.2/calculateroute.xml?app_id=EaMacGi1Wj3dRmQlGXCu&app_code=YXiH50dmZNPxmzS0ldy6Sw&waypoint0=geo!"+lat+","+lng+"&waypoint1=geo!6.911750,79.851406&mode=fastest;car;traffic:disabled", true);
     xhttp.send();
 }
 //get distance from xml file    
@@ -42,6 +58,7 @@ function readDistance(xml) {
     console.log(y);
     //document.getElementById("demo").innerHTML = y.nodeValue; 
 }
+
 //function getRoute(){
    // https://route.api.here.com/routing/7.2/calculateroute.xml?app_id=EaMacGi1Wj3dRmQlGXCu&app_code=YXiH50dmZNPxmzS0ldy6Sw&waypoint0=geo!6.795043,79.900576&waypoint1=geo!6.911750,79.851406&mode=fastest;car;traffic:disabled
 //}
