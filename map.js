97888d4a93ca6893b96f4b958a9e22b895588109
Minul Lamahewage <minullamahewage@gmail.com>
2019-03-07 18:33:13 +0530

    added comments

--- map.js
+++ map.js
@@ -28,6 +28,7 @@ var platform = new H.service.Platform({
 //get current location
 var lat;
 var lng;
+//main function button press
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
@@ -35,6 +36,7 @@ function getLocation() {
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
 }
+//called inside getLocation()
 function showPosition(position) {
     lat = position.coords.latitude;
     lng = position.coords.longitude;
@@ -42,6 +44,7 @@ function showPosition(position) {
   }
 
 //reading xml file
+//main function button press
 function getDistance(){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
@@ -53,7 +56,7 @@ function getDistance(){
     xhttp.open("GET", "https://route.api.here.com/routing/7.2/calculateroute.xml?app_id=EaMacGi1Wj3dRmQlGXCu&app_code=YXiH50dmZNPxmzS0ldy6Sw&waypoint0=geo!"+lat+","+lng+"&waypoint1=geo!6.911750,79.851406&mode=fastest;car;traffic:disabled", true);
     xhttp.send();
 }
-//get distance from xml file    
+//get distance from xml file called inside getDistance()   
 function readDistance(xml) {
     var xmlDoc = xml.responseXML;
     var x = xmlDoc.getElementsByTagName('Distance')[0];
